9fdce1b9c35162b9b31f627baa10ab160290e94e
Zishi-debug <tuzishizhenkeai@gmail.com>
2023-05-01 12:04:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 import * as THREE from '/node_modules/three';
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { RoomEnvironment } from 'node_modules/three/examples/jsm/environments/RoomEnvironment.js';
+import { RoomEnvironment } from '/node_modules/three/examples/jsm/environments/RoomEnvironment.js';
 
 
 (function() {
